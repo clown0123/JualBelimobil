fd6d342c8e0c2c48fbe7d3a3360c4d11f84ea898
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-06-30 16:40:55 +0700

    Create MobilBaru.java

---
+++ MobilBaru.java
@@ -0,0 +1,15 @@
+public class MobilBaru extends Mobil {
+    private String garansi;
+    
+    public MobilBaru(String merek, String model, String garansi){
+        super(merek,model);
+        this.garansi = garansi;
+    }
+    public String getGaransi(){
+        return garansi;
+    }
+    public void info(){
+        System.out.println("Mobil Baru : " + getMerek() + " " + getModel() + " (Garansi: " + garansi + ")" );
+        
+    }
+}
